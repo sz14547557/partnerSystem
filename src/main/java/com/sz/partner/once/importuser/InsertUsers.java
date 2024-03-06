27026112f6add1b2f89cfaaadd0ccf835a6cffb8
sz <14547557@qq.com>
2024-03-06 17:54:59 +0800

    代码执行时间StopWatch类测试

--- src/main/java/com/sz/partner/once/importuser/InsertUsers.java
+++ src/main/java/com/sz/partner/once/importuser/InsertUsers.java
@@ -3,6 +3,7 @@ package com.sz.partner.once.importuser;
 import com.sz.partner.mapper.UserMapper;
 import com.sz.partner.model.domain.User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.util.StopWatch;
 
 import javax.annotation.Resource;
@@ -17,32 +18,42 @@ public class InsertUsers {
 
     @Resource
     private UserMapper userMapper;
+    /**
+     * 盐值，混淆密码
+     */
+    private static final String SALT = "sz";
 
     /**
      * 批量插入用户
      */
 //    @Scheduled(initialDelay = 5000, fixedRate = Long.MAX_VALUE)
     public void doInsertUsers() {
+        // todo sz※ StopWatch 方法测试代码执行时间
         StopWatch stopWatch = new StopWatch();
         System.out.println("goodgoodgood");
+        // 开启计时
         stopWatch.start();
         final int INSERT_NUM = 1000;
         for (int i = 0; i < INSERT_NUM; i++) {
             User user = new User();
-            user.setUsername("假鱼皮");
-            user.setUserAccount("fakeyupi");
+            user.setUsername("某政");
+            user.setUserAccount("14547557"+i);
             user.setAvatarUrl("https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png");
             user.setGender(0);
-            user.setUserPassword("12345678");
-            user.setPhone("123");
+            String encryptPassword = DigestUtils.md5DigestAsHex((SALT + "12345678").getBytes());
+            user.setUserPassword(encryptPassword);
+            user.setPhone("17645359817");
             user.setEmail("123@qq.com");
-            user.setTags("[]");
+            user.setTags("[男]");
             user.setUserStatus(0);
             user.setUserRole(0);
-            user.setPlanetCode("11111111");
+            user.setPlanetCode(String.valueOf(i));
             userMapper.insert(user);
         }
+        // 结束计时
         stopWatch.stop();
+        // 输出执行时间
         System.out.println(stopWatch.getTotalTimeMillis());
     }
+
 }
